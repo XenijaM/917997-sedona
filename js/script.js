ae1c361a6fe49c93396f6207bc62fcdd14c5490a
Your Name <you@example.com>
2018-12-30 02:04:16 +0300

    Скрипт, правки
    1. Доделана форма.
    2. Прописан js.
    3. Внесены правки по результатам тестирования - IE

---
+++ js/script.js
@@ -0,0 +1,28 @@
+  <script>
+    var button = document.querySelector(".search-button");
+    var popup = document.querySelector(".hotel-search");
+    var arrival = popup.querySelector("[name=arrival]");
+    var departure = popup.querySelector("[name=departure]");
+    var form = popup.querySelector("form");
+
+    button.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.toggle("hotel-search-show");
+      arrival.focus();
+    });
+
+      form.addEventListener("submit", function (evt) {
+        if (!arrival.value || !departure.value) {
+        evt.preventDefault();
+        }
+    });
+
+    window.addEventListener("keydown", function (evt){
+      if (evt.keyCode === 27){
+        if (popup.classList.contains ("hotel-search-show")) {
+          evt.preventDefault();
+          popup.classList.remove("hotel-search-show");
+        }
+      }
+    });
+  </script>
